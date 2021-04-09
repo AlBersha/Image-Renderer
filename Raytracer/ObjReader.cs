81fb87a0b3367c28acac6663ab22c93a771358a6
Oleksandra Bershatska <oleksandra.bershatska@solarwinds.com>
2021-04-09 21:17:40 +0300

    simple obj reader added

---
+++ Raytracer/ObjReader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using OBJ3DWavefrontLoader;
+
+namespace Raytracer
+{
+    public static class ObjReader
+    {
+        public static SimpleMesh ReadObjFile(string path)
+        {
+            SimpleMesh simpleMesh;
+            using (var reader = new StreamReader(path))
+            {
+                simpleMesh = SimpleMesh.LoadFromObj(reader);
+            }
+
+            return simpleMesh;
+        }
+    }
+}
